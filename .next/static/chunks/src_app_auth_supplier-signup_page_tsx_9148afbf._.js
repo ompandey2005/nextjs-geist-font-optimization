8b9078ad255ae5884669873734e09e6ed1146ba7
Om Pandey <omsoftec009@gmail.com>
2025-07-27 01:14:55 +0530

    Add .next/static/chunks/src_app_auth_supplier-signup_page_tsx_9148afbf._.js 1753559094675

---
+++ .next/static/chunks/src_app_auth_supplier-signup_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_be5733b9._.js",
+  "static/chunks/src_app_auth_supplier-signup_page_tsx_417eea88._.js"
+],
+    source: "dynamic"
+});
