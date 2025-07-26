84ef647dd392a064a13fb1e84aa1a99b2a5bf478
Om Pandey <omsoftec009@gmail.com>
2025-07-27 01:15:01 +0530

    Add .next/static/chunks/src_app_vendor_analytics_page_tsx_9148afbf._.js 1753559100642

---
+++ .next/static/chunks/src_app_vendor_analytics_page_tsx_9148afbf._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_34635775._.js",
+  "static/chunks/node_modules_2fda90d7._.js",
+  "static/chunks/src_app_vendor_analytics_page_tsx_10d9ea3e._.js"
+],
+    source: "dynamic"
+});
